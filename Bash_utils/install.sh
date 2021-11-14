ce4668359cad2a849271a3f8b68ffe052363023e
IronMatt97 <matti1097@gmail.com>
2021-11-14 19:23:15 +0100

    Progetto completato

---
+++ Bash_utils/install.sh
@@ -0,0 +1,4 @@
+cd ../Discovery/; docker build --tag discovery .
+cd ../DataStore/; docker build --tag datastore .
+cd ../restAPI/; docker build --tag api .
+cd ../Client/; docker build --tag client .
